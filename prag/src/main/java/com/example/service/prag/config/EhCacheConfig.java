dd0ce6f618f4477d86a8b8e47c170514afd42feb
Taehwan2 <taehwansw@gmail.com>
2024-06-12 21:37:26 +0900

    [FETCH] Cache에 대한 Configuration확인

--- prag/src/main/java/com/example/service/prag/config/EhCacheConfig.java
+++ prag/src/main/java/com/example/service/prag/config/EhCacheConfig.java
@@ -20,16 +20,20 @@ import java.time.Duration;
 public class EhCacheConfig {
     @Bean
     public CacheManager getCacheManager() {
+        /*Todo CachingProvider()메서드를 통해서 CachingProviderRegistry 에서 CachingProvider를 가져온다.*/
         CachingProvider provider = Caching.getCachingProvider();
         CacheManager cacheManager = provider.getCacheManager();
 
         CacheConfiguration<String, Object> configuration = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+
+                        // Todo 코드상으로 보면 Cache의 keyType과 Value Type을 String class와 ObjectClass로 지정하고있다.
                         String.class, Object.class, ResourcePoolsBuilder
                                 .heap(100)
                                 .offheap(10, MemoryUnit.MB))
                 .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(10)))
                 .build();
 
+        /* Todo Eh107Configuration을 사용하면, JCache의 표준 API와 Ehcache의 성능을 모두 활용할 수 있습니다.  */
         javax.cache.configuration.Configuration<String, Object> cacheConfiguration
                 = Eh107Configuration.fromEhcacheCacheConfiguration(configuration);
 
